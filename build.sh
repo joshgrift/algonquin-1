cd929dcdc14c7979f5517c82a87f22e734385e61
joshgrift <josh@grift.ca>
2026-01-03 17:31:13 -0800

    Added Zip Command for upload

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ VERSION=$(grep 'config/version=' project.godot | sed 's/config\/version="\(.*\)"
 
 echo ==== dist/algonquin-$VERSION.dmg ====
 /Applications/Godot_mono.app/Contents/MacOS/Godot --headless --export-debug "macOS" dist/algonquin-$VERSION.app
+zip -r "dist/algonquin-$VERSION.zip" "dist/algonquin-$VERSION.app"
 
 echo ==== dist/algonquin-server-$VERSION.dmg ====
 /Applications/Godot_mono.app/Contents/MacOS/Godot --headless --export-debug "macOS-server" dist/algonquin-server-$VERSION.app
+t-debug "macOS-server" dist/algonquin-server-$VERSION.app
+zip -r "dist/algonquin-server-$VERSION.zip" "dist/algonquin-server-$VERSION.app"
